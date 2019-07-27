7ad71442aedc295d499bf84f63242e3d3d0cb678
TaTaTaZJJ <48116013+TaTaTaZJJ@users.noreply.github.com>
2019-07-27 23:05:57 +0800

    Decorate bag

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -439,7 +439,7 @@ $(TYPESGFXDIR)/move_types.gbapal: $(TYPESGFXDIR)/move_types_1.gbapal \
 	@cat $^ >$@
 
 $(INTERFACEGFXDIR)/bag_screen.4bpp: %.4bpp: %.png
-	$(GFX) $< $@ -num_tiles 53
+	$(GFX) $< $@ -num_tiles 79
 
 $(RAYQUAZAGFXDIR)/rayquaza.8bpp: %.8bpp: %.png
 	$(GFX) $< $@ -num_tiles 227
